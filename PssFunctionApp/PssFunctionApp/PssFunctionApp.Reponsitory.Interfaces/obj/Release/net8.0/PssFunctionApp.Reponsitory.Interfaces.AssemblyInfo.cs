1e7c6fab2fe7ad2dd98b6e43074298e900abc510
Nguyen Huu Tu Hai <hainh@bsdinsight.com>
2025-04-04 09:39:45 +0700

    up cache sau khi pull lại file

--- PssFunctionApp/PssFunctionApp/PssFunctionApp.Reponsitory.Interfaces/obj/Release/net8.0/PssFunctionApp.Reponsitory.Interfaces.AssemblyInfo.cs
+++ PssFunctionApp/PssFunctionApp/PssFunctionApp.Reponsitory.Interfaces/obj/Release/net8.0/PssFunctionApp.Reponsitory.Interfaces.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PssFunctionApp.Reponsitory.Interfaces")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e985e3c403748e848d9550d3a7f350fa23e064c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdf4483e6918c963c5f62ca2e024ee4ede0f4dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PssFunctionApp.Reponsitory.Interfaces")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PssFunctionApp.Reponsitory.Interfaces")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
