bd9cdd87cecdfdd574be9c97fa4b23f66aa2092f
nguyentansongbsd <songnt@bsdinsight.com>
2026-04-01 17:44:32 +0700

    sync Utop

---
+++ Plugin_UtopSyncContract/Plugin_UtopSyncContract/Plugin_UtopSyncContract.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin_UtopSyncContract
+{
+    public class Plugin_UtopSyncContract : IPlugin
+    {
+        IOrganizationService service = null;
+        IOrganizationServiceFactory serviceFactory = null;
+        IPluginExecutionContext context = null;
+        ITracingService tracingService = null;
+
+        Entity target = null;
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            this.context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            this.serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            this.service = serviceFactory.CreateOrganizationService(context.UserId);
+            this.tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            Init();
+        }
+        private async Task Init()
+        {
+            if (this.context.MessageName == "Delete")
+                return;
+            if (this.context.Depth > 3)
+                return;
+            this.target = this.context.InputParameters["Target"] as Entity;
+            Entity enContract = service.Retrieve(this.target.LogicalName, this.target.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet("customerid"));
+            Entity enCustomer = service.Retrieve(((EntityReference)enContract["customerid"]).LogicalName, ((EntityReference)enContract["customerid"]).Id, new Microsoft.Xrm.Sdk.Query.ColumnSet("bsd_isconsent"));
+            if (!enCustomer.Contains("bsd_isconsent") || (enCustomer.Contains("bsd_isconsent") && (bool)enCustomer["bsd_isconsent"] == false))
+                return;
+
+            // call api azure function to sync project data to utop system
+            string url = $@"https://functionapp-cldvncapitaone-prod-fdezg4fwgphzcuef.southeastasia-01.azurewebsites.net/api/upsertcontract?id={enCustomer.Id}&entity={enCustomer.LogicalName}";
+            HttpClient httpClient = new HttpClient();
+
+            var respose = await httpClient.GetAsync(url);
+            if (respose.IsSuccessStatusCode)
+            {
+                tracingService.Trace("Sync data to utop system successfully.");
+            }
+            else
+            {
+                tracingService.Trace("Sync data to utop system failed.");
+            }
+        }
+    }
+}
---
+++ Plugin_UtopSyncInstallment/Plugin_UtopSyncInstallment/Plugin_UtopSyncInstallment.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin_UtopSyncInstallment
+{
+    public class Plugin_UtopSyncInstallment : IPlugin
+    {
+        IOrganizationService service = null;
+        IOrganizationServiceFactory serviceFactory = null;
+        IPluginExecutionContext context = null;
+        ITracingService tracingService = null;
+
+        Entity target = null;
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            this.context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            this.serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            this.service = serviceFactory.CreateOrganizationService(context.UserId);
+            this.tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            Init();
+        }
+        private async Task Init()
+        {
+            if (this.context.MessageName == "Delete")
+                return;
+            if (this.context.Depth > 3)
+                return;
+            this.target = this.context.InputParameters["Target"] as Entity;
+            Entity enInstallment = service.Retrieve(this.target.LogicalName, this.target.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet("bsd_optionentry"));
+            Entity enContact = service.Retrieve(((EntityReference)enInstallment["bsd_optionentry"]).LogicalName, ((EntityReference)enInstallment["bsd_optionentry"]).Id, new Microsoft.Xrm.Sdk.Query.ColumnSet("bsd_isconsent"));
+            if (!enContact.Contains("bsd_isconsent") || (enContact.Contains("bsd_isconsent") && (bool)enContact["bsd_isconsent"] == false))
+                return;
+
+            // call api azure function to sync project data to utop system
+            string url = $@"https://functionapp-cldvncapitaone-prod-fdezg4fwgphzcuef.southeastasia-01.azurewebsites.net/api/upsertcontract?id={this.target.Id}&entity={this.target.LogicalName}";
+            HttpClient httpClient = new HttpClient();
+
+            var respose = await httpClient.GetAsync(url);
+            if (respose.IsSuccessStatusCode)
+            {
+                tracingService.Trace("Sync data to utop system successfully.");
+            }
+            else
+            {
+                tracingService.Trace("Sync data to utop system failed.");
+            }
+        }
+
+    }
+}
