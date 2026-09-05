2defcf236b3cc6f7b4114b316547f82b26d7d61f
Thinh Le <thinhld@bsdinsight.com>
2026-09-05 15:05:55 +0700

    thinh up code

---
+++ Action_Retransfer_Contact_Account/Action_Retransfer_Contact_Account/Action_Retransfer_Contact_Account.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_Retransfer_Contact_Account
+{
+    public class Action_Retransfer_Contact_Account : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            try
+            {
+                // 1. Ép hệ thống dùng TLS 1.2 (Bắt buộc đối với Azure)
+                System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+                // 2. Lấy tham số truyền từ JS
+                string targetId = context.InputParameters.Contains("TargetId") ? context.InputParameters["TargetId"].ToString() : "";
+                string entityName = context.InputParameters.Contains("EntityName") ? context.InputParameters["EntityName"].ToString() : "";
+                string environment = context.InputParameters.Contains("Environment") ? context.InputParameters["Environment"].ToString() : "";
+
+                // 3. Khai báo URL (Thêm Function Key nếu Azure có yêu cầu)
+                string url = $"https://functionapp-cldvncapitaone-prod-fdezg4fwgphzcuef.southeastasia-01.azurewebsites.net/api/{environment}/upsertcontract?id={targetId}&entity={entityName}";
+
+                tracing.Trace($"Goi URL: {url}");
+
+                // 4. Thực hiện Request
+                using (HttpClient client = new HttpClient())
+                {
+                    // Set Timeout tránh ngâm connection
+                    client.Timeout = TimeSpan.FromSeconds(30);
+
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+                    string responseContent = response.Content.ReadAsStringAsync().Result;
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        throw new InvalidPluginExecutionException($"Azure Function tra ve loi ({response.StatusCode}): {responseContent}");
+                    }
+
+                    // 5. Trả kết quả về cho JS
+                    context.OutputParameters["ResponseResult"] = responseContent;
+                }
+            }
+            catch (Exception ex)
+            {
+                tracing.Trace("Loi Plugin: {0}", ex.ToString());
+                // Quăng lỗi ra UI để biết chính xác nguyên nhân
+                throw new InvalidPluginExecutionException("Loi ket noi Azure Function: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Action_Retransfer_Contact_Account/Action_Retransfer_Contact_Account/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Action_Retransfer_Contact_Account")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Action_Retransfer_Contact_Account")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4fbf0efa-6b6c-4998-b0ec-0bbf50176566")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
